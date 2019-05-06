503a25324acd38f5c4767b4fb0999977162e2a5b
rinao <180233999@qq.com>
2019-05-06 21:43:32 +0800

    update

--- app/redux/actions/menuAction.js
+++ app/redux/actions/menuAction.js
@@ -1,7 +1,31 @@
 export const ADD = "ADD";
+export const SUB = "SUB";
+export const DEL = "DEL";
+export const CLEAR = "CLEAR";
 
-export function add() {
+export function add(params) {
     return {
-        type: ADD
+        type: ADD,
+        params: params
+    };
+}
+
+export function sub(params) {
+    return {
+        type: SUB,
+        params: params
+    };
+}
+
+export function del(params) {
+    return {
+        type: DEL,
+        params: params
+    };
+}
+
+export function clear() {
+    return {
+        type: CLEAR
     };
 }
--- app/redux/reducers/menuReducer.js
+++ app/redux/reducers/menuReducer.js
@@ -1,16 +1,39 @@
-import { ADD } from "../actions/menuAction";
+import { ADD, SUB, DEL, CLEAR } from "../actions/menuAction";
 
 const initialState = {
-    count: 0
+    menuList: []
 };
 
 function menuReducer(state = initialState, action) {
     switch (action.type) {
         case ADD:
-            return Object.assign({}, state, { count: state.count + 1 });
+            return add(state, action.params);
+        case SUB:
+            return sub(state, action.params);
+        case DEL:
+            return del(state, action.params);
+        case CLEAR:
+            return clear(state);
         default:
             return state;
     }
+}
+
+function add(state, params) {
+    // skuId, favorIds, goodsName, skuName, price
+    return Object.assign({}, state, { count: state.count + 1 });
+}
+
+function sub(state, params) {
+
+}
+
+function del(state, params) {
+
+}
+
+function clear(state) {
+    return Object.assign({}, state, { menuList: [] });
 }
 
 export default menuReducer;
